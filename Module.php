cb111d1155e1804fc2299a79de8f3acaeafe4700
Sandro Keil <sandro_keil@web.de>
2014-05-03 17:44:50 +0100

    added Module.php for modules.zendframework.com

---
+++ Module.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Sake
+ *
+ * @link      http://github.com/sandrokeil/HtmlElement for the canonical source repository
+ * @copyright Copyright (c) 2014 Sandro Keil
+ * @license   http://github.com/sandrokeil/HtmlElement/blob/master/LICENSE.txt New BSD License
+ */
+
+require_once __DIR__ . '/src/Module.php';
